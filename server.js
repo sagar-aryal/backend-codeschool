dac6a1ffd24b808b6b16e5eaf3757d9023c6a162
sagar-aryal <sagar_yal92@outlook.com>
2022-05-21 14:58:23 +0300

    creating logger middleware vs morgan

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
 
 import bootcamps from "./routes/bootcamps.js";
+import { logger } from "./middleware/logger.js";
 
 // Load env variables
 dotenv.config({ path: "./config/config.env" });
@@ -10,6 +12,10 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+// Logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 // Mount Routers
 app.use("/api/v1/bootcamps", bootcamps);
 
