f9e34b67a86b62f6d75a4722ff6d6be1b7f5b569
sagar-aryal <sagar_yal92@outlook.com>
2022-05-21 11:50:40 +0300

    http get and post methods

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const http = require("http");
 
+const port = 5000;
+
 const todos = [
   {
     id: 1,
@@ -15,18 +17,49 @@ const server = http.createServer((req, res) => {
   /* console.log(req);
   const { headers, url, method } = req;
   console.log(headers, url, method); */
-  res.setHeader("Content-Type", "application/json");
-  res.writeHead(200);
-  /*  res.write("Hello backend is here."); */
-  res.end(
-    JSON.stringify({
-      success: true,
-      data: todos,
-    })
-  );
-});
 
-const port = 5000;
+  const { method, url } = req;
+  let body = [];
+
+  req
+    .on("data", (chunk) => {
+      body.push(chunk);
+    })
+    .on("end", () => {
+      body = Buffer.concat(body).toString();
+      // console.log(body);
+
+      let status = 404;
+      const response = {
+        success: false,
+        data: null,
+        error: null,
+      };
+
+      if (method === "GET" && url === "/todos") {
+        status = 200;
+        response.success = true;
+        response.data = todos;
+      } else if (method === "POST" && url === "/todos") {
+        const { id, text } = JSON.parse(body);
+
+        if (!id || !text) {
+          status = 400;
+          response.error = "Please add id and text";
+        } else {
+          todos.push({ id, text });
+          status = 201;
+          response.success = true;
+          response.data = todos;
+        }
+      }
+
+      res.writeHead(status, { "Content-Type": "application/json" });
+      /*  res.write("Hello backend is here."); */
+
+      res.end(JSON.stringify(response));
+    });
+});
 
 server.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
