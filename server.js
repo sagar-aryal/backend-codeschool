a355380009bc1d0eeeb396784509259de1a0698f
sagar-aryal <sagar_yal92@outlook.com>
2022-05-21 14:01:00 +0300

    creating routes for bootcamps

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import bootcamps from "./routes/bootcamps.js";
+
 // Load env variables
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+// Mount Routers
+app.use("/api/v1/bootcamps", bootcamps);
 
 app.listen(port, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
