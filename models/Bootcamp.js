c1a6225b969f4d7d9e1f92b26db75a0e330554b5
sagar-aryal <sagar_yal92@outlook.com>
2022-05-23 00:00:58 +0300

    creating slug from name using slugify

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import slugify from "slugify";
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -97,6 +98,12 @@ const BootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now(),
   },
+});
+
+// Create bootcamp slug from the name
+BootcampSchema.pre("save", (next) => {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 export default mongoose.model("Bootcamp", BootcampSchema);
