683729cbcd2f8cd32837c7b77a314444fc254754
sagar-aryal <sagar_yal92@outlook.com>
2022-05-30 18:54:18 +0300

    created json data and seeder file to import and delete those data in db

--- api/config/db.js
+++ api/config/db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI);
+
   console.log(`MongoDB is connected: ${conn.connection.host}`);
 };
 
---
+++ api/seeder.js
@@ -0,0 +1,55 @@
+import fs from "fs";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+import Bootcamp from "./models/Bootcamp.js";
+import Course from "./models/Course.js";
+
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// load env vars
+dotenv.config({ path: "./config/config.env" });
+
+// connect to DB
+const conn = await mongoose.connect(process.env.MONGO_URI);
+
+// read JSON files
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
+);
+
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
+);
+
+// import into DB
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+    await Course.create(courses);
+    console.log("Data imported from DB");
+    process.exit();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// delete data from DB
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+    await Course.deleteMany();
+    console.log("Data deleted from DB");
+    process.exit();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+if (process.argv[2] === "-i") {
+  importData();
+} else if (process.argv[2] === "-d") {
+  deleteData();
+}
